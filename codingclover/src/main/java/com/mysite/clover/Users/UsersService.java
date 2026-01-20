0c06b89e6b5cfa74a3325cd3264269c7fc7af478
wnstj999 <wnstj99999@gmail.com>
2026-01-20 09:01:36 +0900

    유저서비스 수정

--- codingclover/src/main/java/com/mysite/clover/Users/UsersService.java
+++ codingclover/src/main/java/com/mysite/clover/Users/UsersService.java
@@ -11,18 +11,13 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public Users create(String loginId, String password, String nickname, String name, String email, String phone, String birthDate, String gender) {
+    public Users create(String loginId, String password, String name, String email) {
         Users user = new Users();
         user.setLoginId(loginId);
         user.setPassword(passwordEncoder.encode(password));
-        user.setNickname(nickname);
         user.setName(name);
         user.setEmail(email);
-        user.setPhone(phone);
-        user.setBirthDate(birthDate);
-        user.setGender(gender);
-        user.setRole(UsersRole.STUDENT); // 기본값 설정
-        // 필요한 다른 필드 설정
+        user.setRole(UsersRole.STUDENT);
         this.usersRepository.save(user);
         return user;
     }
