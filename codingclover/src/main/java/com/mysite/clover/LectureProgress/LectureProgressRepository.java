f952a5b28741a185ad4259a0339967ecb56d3679
parknahye-dot <a207903@gmail.com>
2026-01-22 14:15:05 +0900

    원복

--- codingclover/src/main/java/com/mysite/clover/LectureProgress/LectureProgressRepository.java
+++ codingclover/src/main/java/com/mysite/clover/LectureProgress/LectureProgressRepository.java
@@ -1,17 +1,5 @@
 package com.mysite.clover.LectureProgress;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysite.clover.Enrollment.Enrollment;
-import com.mysite.clover.Lecture.Lecture;
-
-public interface LectureProgressRepository extends JpaRepository<LectureProgress, Long> {
-    List<LectureProgress> findByEnrollment(Enrollment enrollment);
-
-    Optional<LectureProgress> findByEnrollmentAndLecture(Enrollment enrollment, Lecture lecture);
-
-    long countByEnrollmentAndCompletedYnTrue(Enrollment enrollment);
+public class LectureProgressRepository {
+  
 }
