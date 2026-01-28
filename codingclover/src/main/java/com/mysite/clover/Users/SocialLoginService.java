cfd885f42b8f5b0b871ef448a65999ba6ef3e117
wnstj999 <wnstj99999@gmail.com>
2026-01-29 00:22:07 +0900

    카카오 로그인 구현 완료

--- codingclover/src/main/java/com/mysite/clover/Users/SocialLoginService.java
+++ codingclover/src/main/java/com/mysite/clover/Users/SocialLoginService.java
@@ -1,11 +1,13 @@
 package com.mysite.clover.Users;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-// 구글 로그인
 public class SocialLoginService extends DefaultOAuth2UserService {
 
     private final UsersRepository usersRepository;
@@ -22,24 +23,68 @@ public class SocialLoginService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
+        // 어떤 소셜인지 구별함
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        String email = (String) attributes.get("email");
-        String name = (String) attributes.get("name");
+        String providerId = null;
+        String email = null;
+        String name = null;
 
-        Users user = usersRepository.findByEmail(email)
+        // 구글일때
+        if ("google".equals(registrationId)) {
+            providerId = (String) attributes.get("sub"); // 구글 ID는 문자열
+            email = (String) attributes.get("email");
+            name = (String) attributes.get("name");
+        }
+        // 카카오일때
+        else if ("kakao".equals(registrationId)) {
+            // 카카오 id는 숫자여서 문자열로 바꾼다는데.. 왜지?
+            providerId = String.valueOf(attributes.get("id"));
+            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+            if (kakaoAccount != null) {
+                email = (String) kakaoAccount.get("email");
+                Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+                if (profile != null) {
+                    name = (String) profile.get("nickname");
+                }
+
+                if (name == null) {
+                    Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+                    if (properties != null) {
+                        name = (String) properties.get("nickname");
+                    }
+                }
+            }
+        }
+
+        String loginId = registrationId + "_" + providerId;
+
+        // 사용자가 카카오 로그인 할때 이메일 제공을 거부했을 시에 임시로라도 이메일 주소 만듬
+        String finalEmail = (email != null) ? email : loginId + "@codingclover.com";
+        String finalName = (name != null) ? name : "Unknown";
+
+        Users user = usersRepository.findByEmail(finalEmail)
                 .orElseGet(() -> {
                     Users newUser = new Users();
-                    newUser.setLoginId(registrationId + "_" + attributes.get("sub"));
-                    newUser.setPassword(UUID.randomUUID().toString()); 
-                    newUser.setName(name);
-                    newUser.setEmail(email);
+                    newUser.setLoginId(loginId);
+                    newUser.setPassword(UUID.randomUUID().toString());
+                    newUser.setName(finalName);
+                    newUser.setEmail(finalEmail);
                     newUser.setRole(UsersRole.STUDENT);
                     newUser.setStatus(UsersStatus.ACTIVE);
                     return usersRepository.save(newUser);
                 });
 
-        return oAuth2User;
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        Map<String, Object> newAttributes = new HashMap<>(attributes);
+        newAttributes.put("email", finalEmail);
+
+        return new DefaultOAuth2User(
+                oAuth2User.getAuthorities(),
+                newAttributes,
+                userNameAttributeName);
     }
 }
