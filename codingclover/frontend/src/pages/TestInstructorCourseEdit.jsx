d4af71c47ac8a18cb218b9b8b5c8cf79f074c3c7
dlawjd65 <dlawjd65@gmail.com>
2026-01-29 16:08:34 +0900

    에러 방지용 내용 추가

---
+++ codingclover/frontend/src/pages/TestInstructorCourseEdit.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "@/components/Nav";
+import Tail from "@/components/Tail";
+
+function TestInstructorCourseEdit() {
+    return (
+        <>
+        <Nav />
+
+        <Tail />
+        </>
+    )
+}
+
+export default TestInstructorCourseEdit;
---
+++ codingclover/frontend/src/pages/TestInstructorLectureEdit.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "@/components/Nav";
+import Tail from "@/components/Tail";
+
+function TestInstructorLectureEdit() {
+    return (
+        <>
+        <Nav />
+
+        <Tail />
+        </>
+    )
+}
+
+export default TestInstructorLectureEdit;
